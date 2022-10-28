2ce46cb1feb26c53be1c940fca2396ae682a693d
Toby Inkster <mail@tobyinkster.co.uk>
2022-10-28 15:08:49 +0100

    clippy doesn't like variables called foo, so rename the variable, and the file while we're at it

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,8 @@ use crate::tadalist::*;
 use std::fs::File;
 
 fn main() {
-	/*
-	let i = TadaItem::parse("x (A) 2000-01-01 Foo bar @baz");
-	println!("{:?}", i);
-	println!("{}", i);
-	*/
-
-	let foo = File::open("foo.txt");
-	let l = TadaList::new_from_file(foo.unwrap());
+	let testfile = File::open("example-todo.txt");
+	let l = TadaList::new_from_file(testfile.unwrap());
 
 	println!("{:?}", l);
 }
