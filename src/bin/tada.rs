d70457167374f81bf5a9eee9c2124034c969b36f
Toby Inkster <mail@tobyinkster.co.uk>
2022-11-14 18:13:07 +0000

    There doesn't seem to be a point in having a separate cli() function

--- src/bin/tada.rs
+++ src/bin/tada.rs
@@ -2,19 +2,7 @@
 use clap::Command;
 use tada::Action;
 
-fn cli() -> Command {
-	let mut cmd = Command::new("tada")
-		.version("0.1.0")
-		.about("A todo list manager")
-		.subcommand_required(true)
-		.term_width(80)
-		.allow_external_subcommands(true);
-	for action in actions() {
-		cmd = cmd.subcommand(action.command);
-	}
-	cmd
-}
-
+/// Get a list of valid subcommands.
 fn actions() -> Vec<Action> {
 	Vec::from([
 		tada::action::add::get_action(),
@@ -22,8 +10,20 @@ fn actions() -> Vec<Action> {
 	])
 }
 
+/// Main body of the `tada` command.
 fn main() {
-	let matches = cli().get_matches();
+	let mut cmd = Command::new("tada")
+		.version("0.1.0")
+		.about("A todo list manager")
+		.subcommand_required(true)
+		.term_width(80)
+		.allow_external_subcommands(true);
+
+	for action in actions() {
+		cmd = cmd.subcommand(action.command);
+	}
+
+	let matches = cmd.get_matches();
 	match matches.subcommand() {
 		Some(("add", args)) => tada::action::add::execute(args),
 		Some(("show", args)) => tada::action::show::execute(args),
