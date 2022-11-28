b94e2c95f3e7ef2c7f89637effa55f25b69fe0c0
Toby Inkster <mail@tobyinkster.co.uk>
2022-11-28 09:23:00 +0000

    Minor refactoring of edit subcommand to make it more testable; add tests for it.

--- src/action/edit.rs
+++ src/action/edit.rs
@@ -1,7 +1,6 @@
 use crate::action::*;
 use clap::{ArgMatches, Command};
-use std::env;
-use std::process::Command as SysCommand;
+use std::{env, io, process};
 
 /// Options for the `edit` subcommand.
 pub fn get_action() -> Action {
@@ -14,15 +13,12 @@ pub fn get_action() -> Action {
 }
 
 /// Execute the `edit` subcommand.
+#[cfg(not(tarpaulin_include))]
 pub fn execute(args: &ArgMatches) {
-	let filename = FileType::TodoTxt.filename(args);
 	let editor =
 		editor().unwrap_or_else(|_| panic!("Could not determine EDITOR"));
-
-	let mut cmd = SysCommand::new(editor);
-	let cmd = cmd.args([filename]);
-	let mut child = cmd.spawn().unwrap();
-	child.wait().unwrap();
+	let filename = FileType::TodoTxt.filename(args);
+	open_file_in_editor(editor, filename).unwrap();
 }
 
 /// Figure out the editor to use based on the environment.
@@ -33,4 +29,56 @@ pub fn editor() -> Result<String, env::VarError> {
 		Err(error) => return Err(error),
 	}
 	Ok("vi".to_string())
+}
+
+/// Open a file in an editor.
+pub fn open_file_in_editor(
+	editor: String,
+	filename: String,
+) -> Result<process::ExitStatus, io::Error> {
+	let mut cmd = process::Command::new(editor);
+	let cmd = cmd.args([filename]);
+	let mut child = cmd.spawn()?;
+	child.wait()
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use tempfile::tempdir;
+
+	#[test]
+	fn test_get_action() {
+		assert_eq!(String::from("edit"), get_action().name);
+	}
+
+	#[test]
+	fn test_editor() {
+		env::set_var("EDITOR", "cat");
+		assert!(editor().is_ok());
+		assert_eq!("cat", editor().unwrap());
+	}
+
+	#[test]
+	fn test_open_file_in_editor() {
+		let dir = tempdir().unwrap();
+		let test_filename = dir
+			.path()
+			.join("some-file.txt")
+			.display()
+			.to_string();
+
+		// `cat FILE` with a non-existing file.
+		let exitcode =
+			open_file_in_editor(String::from("cat"), test_filename.clone())
+				.unwrap();
+		assert!(!exitcode.success());
+
+		// `cat FILE` with an existing file.
+		List::new().to_filename(test_filename.clone());
+		let exitcode =
+			open_file_in_editor(String::from("cat"), test_filename.clone())
+				.unwrap();
+		assert!(exitcode.success());
+	}
 }
